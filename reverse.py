5319fffe9cefbd6527f79717f26e97198324ead7
swaathe ganesan <36266081+swaathe@users.noreply.github.com>
2018-02-14 19:16:40 +0530

    rev
    to reverse a string

---
+++ reverse.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Swaathe
+#
+# Created:     14-02-2018
+# Copyright:   (c) Swaathe 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    s=input("enter the string to reverse")
+    print(s[::-1])
+
+if __name__ == '__main__':
+    main()
