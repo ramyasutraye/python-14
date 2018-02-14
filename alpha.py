d26595016ff7b59159f0119bebf23cd1f3b5abe8
swaathe ganesan <36266081+swaathe@users.noreply.github.com>
2018-02-14 16:26:31 +0530

    letters
    to check a letter

--- alpha.py
+++ alpha.py
@@ -1,9 +1,5 @@
-while True:
-	ch = input("Enter any character: ")
-	if ch == '0':
-		break
-	else:
-		if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+ch = input("Enter any character: ")	
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
 			print(ch, "is an alphabet.\n")
-		else:
-			print(ch, "given num is not an alphabet.\n")
+else:
+	print(ch, "given num is not an alphabet.\n")
