64555f64d1a5ba86ac1f5c3d82a4c56578d0d88e
swaathe ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-01 16:07:11 +0530

    max nd min element in array

---
+++ maxmin.py
@@ -0,0 +1,4 @@
+n = [int(x) for x in input("enter the array element").split()]
+n.sort()
+print(max(n),("is the max element in the given array"))
+print(min(n),("is the min element in the given array"))
