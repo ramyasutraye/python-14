0239c630894d32104979d88d94f272163679e050
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-03 10:31:51 +0530

    tf pdt of 2 nd chk evn

---
+++ pdt2numssx.py
@@ -0,0 +1,7 @@
+n = int (input("enter the frst num:"))
+m =int(input("enter the second num:"))
+print (n*m)
+if (n*m %2==0):
+    print(n*m,("is an even num"))
+else:
+    print(n*m,("is a odd num"))
