bf93c2f2f93e49d8a0d2a0ce11d8bf531fa371cd
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-03 17:10:12 +0530

    tf even value

---
+++ neartesteven.py
@@ -0,0 +1,7 @@
+try:
+	n=int(input("enter a value"))
+	if (n%2!=0):
+		n-=1
+		print(n)
+except:
+	print('invalid')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
