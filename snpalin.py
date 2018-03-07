02019b12f5a4ca705cf06197c7a00271fe70682f
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-07 17:26:46 +0530

    Update snpalin.py

--- snpalin.py
+++ snpalin.py
@@ -6,7 +6,6 @@ while(n>0):
     rev=rev*10+num
     n=n//10
 if(temp==rev):
-    print("The number is a palindrome")
+    print(temp," is a palindrome")
 else:
-    print("The number isn't a palindrome")
-
+    print(temp," isn't a palindrome")
