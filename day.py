0f7b2b273c27346a7042fb5eefae05c7f3c32277
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-08 19:05:07 +0530

    tf holiday

---
+++ day.py
@@ -0,0 +1,5 @@
+a=str(input("enter the day"))
+if(a=='SUNDAY' or a=='Sunday' or a=='sunday'or a=='SATURDAY' or a=='Saturday' or a=='saturday'):
+    print(a,"is a Holiday")
+else:
+    print(a,"is a Working day")
