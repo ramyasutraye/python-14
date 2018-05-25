2350b5d90a95ea67269064a457585bdffe5bb68b
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-05-25 09:46:26 +0530

    tf repeated

---
+++ bgfindrepeat.py
@@ -0,0 +1,4 @@
+from collections import Counter
+n=[int(x) for x in input("enter the elements in  list1:").split()]
+k=[int(x) for x in input("enter the elements in list2:").split()]
+print(Counter(k))
